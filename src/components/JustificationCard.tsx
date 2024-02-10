e8b4813aea3a5992bed4590cd95144b5f6963271
sysung <sysung98@berkeley.edu>
2024-02-10 10:15:06 -0600

    Create veracity score and justification

--- src/components/JustificationCard.tsx
+++ src/components/JustificationCard.tsx
@@ -7,10 +7,30 @@ export default function JustificationCard() {
     <div className="pt-2">
       <Card variant="outlined">
         <CardContent>
-          <Typography>
-            This is a sentence.<br></br>
-            This is another sentence.
+          <Typography className="pb-4" variant="h5">
+            Veracity Score: 70
           </Typography>
+          <div className="h-[285px] overflow-auto">
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Hac habitasse platea dictumst vestibulum rhoncus est pellentesque
+              elit ullamcorper. Tristique et egestas quis ipsum suspendisse ultrices.
+              Felis eget nunc lobortis mattis aliquam faucibus purus in.
+              Pellentesque nec nam aliquam sem et tortor consequat id porta.
+              Laoreet non curabitur gravida arcu ac tortor.
+              Fusce id velit ut tortor pretium viverra suspendisse.
+              Faucibus scelerisque eleifend donec pretium vulputate sapien nec
+              sagittis aliquam. Non diam phasellus vestibulum lorem sed risus. Diam
+              in arcu cursus euismod quis viverra. Facilisi cras fermentum odio
+              eu feugiat pretium nibh. Quis lectus nulla at volutpat diam. 
+              Nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus. 
+              Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+              Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. 
+              Tellus in metus vulputate eu scelerisque felis imperdiet. 
+              Risus ultricies tristique nulla aliquet enim tortor at.
+            </Typography>
+          </div>
         </CardContent>
       </Card>
     </div>
