d643e2ee3f68da5d1d5d527999ce3e20e4c095a7
Steven Sung <sysung98@gmail.com>
2024-02-19 23:30:00 -0600

    Change spacing to two spaces

--- src/components/VeracityScoreBar.tsx
+++ src/components/VeracityScoreBar.tsx
@@ -1,22 +1,21 @@
 import { LinearProgress, Typography } from "@mui/material";
 
 interface VeracityScoreProps {
-    veracityScore: number; //Progress value between 0 and 100
+  veracityScore: number; //Progress value between 0 and 100
 }
 
 const VeracityScoreBar: React.FC<VeracityScoreProps> = ({ veracityScore }) => {
-
-    return (
-        <>
-            <Typography className="flex justify-center" variant="overline">
-                Veracity Score
-            </Typography>
-            <Typography className="flex justify-center pb-2" variant="h5">
-                {veracityScore}
-            </Typography>
-            <LinearProgress variant="determinate" value={veracityScore} />
-        </>
-    );
+  return (
+    <>
+      <Typography className="flex justify-center" variant="overline">
+        Veracity Score
+      </Typography>
+      <Typography className="flex justify-center pb-2" variant="h5">
+        {veracityScore}
+      </Typography>
+      <LinearProgress variant="determinate" value={veracityScore} />
+    </>
+  );
 }
 
 export default VeracityScoreBar; 
